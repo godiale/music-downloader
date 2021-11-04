d6a0de3aa35acae7ca1e4ea6fef28c9e6c99c9df
godiale <11892279+godiale@users.noreply.github.com>
2021-11-04 21:57:12 +0300

    Save mp3 from dnbradio by url

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import urllib.request
+from datetime import datetime
+from pathlib import Path
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+FILE_URL = 'https://dnbradio.com/podcast/dl/livesets/' \
+           'ritchey_LIVE_on_DNBRADIO.COM_20141205_0900_-_coffee_n_bass__120514__cup001.mp3'
+SAVE_DIR = 'C:/Users/godin/Music'
 
 
-# Press the green button in the gutter to run the script.
+def extract_file_name(url):
+    return url.split(sep='/')[-1]
+
+
+def download_file(url, save_dir):
+    file_name = extract_file_name(url)
+    print(f"{datetime.now()} Start downloading {file_name}")
+    full_path = Path(save_dir) / file_name
+    urllib.request.urlretrieve(url, full_path)
+    print(f"{datetime.now()} Finish downloading {file_name}")
+
+
+def init_urllib():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+
+def main():
+    init_urllib()
+    Path(SAVE_DIR).mkdir(parents=True, exist_ok=True)
+    download_file(FILE_URL, SAVE_DIR)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
